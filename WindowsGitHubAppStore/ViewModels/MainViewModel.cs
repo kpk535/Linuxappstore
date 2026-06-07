9274da881379d1a4c54b6762ca6bcbe190ae345c
kpk535 <sgkpk6236@gmail.com>
2026-06-07 12:19:41 +0930

    Upgrade view model with store actions

--- WindowsGitHubAppStore/ViewModels/MainViewModel.cs
+++ WindowsGitHubAppStore/ViewModels/MainViewModel.cs
@@ -1,6 +1,7 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows;
 using System.Windows.Input;
 using WindowsGitHubAppStore.Commands;
 using WindowsGitHubAppStore.Helpers;
@@ -12,7 +13,9 @@ namespace WindowsGitHubAppStore.ViewModels;
 public class MainViewModel : INotifyPropertyChanged
 {
     private readonly GitHubService _service = new();
-    private string _searchText = "video player";
+    private string _searchText = "windows app";
+    private string _statusText = "Ready. Search GitHub apps and tools.";
+    private bool _isLoading;
 
     public string SearchText
     {
@@ -20,27 +23,65 @@ public class MainViewModel : INotifyPropertyChanged
         set { _searchText = value; OnPropertyChanged(); }
     }
 
+    public string StatusText
+    {
+        get => _statusText;
+        set { _statusText = value; OnPropertyChanged(); }
+    }
+
+    public bool IsLoading
+    {
+        get => _isLoading;
+        set { _isLoading = value; OnPropertyChanged(); }
+    }
+
     public ObservableCollection<GitHubRepository> Repositories { get; } = new();
 
     public ICommand SearchCommand { get; }
     public ICommand OpenCommand { get; }
+    public ICommand DownloadCommand { get; }
+    public ICommand CopyCloneCommand { get; }
+    public ICommand CategoryCommand { get; }
 
     public MainViewModel()
     {
         SearchCommand = new RelayCommand(async _ => await SearchAsync());
-        OpenCommand = new RelayCommand(r =>
+        OpenCommand = new RelayCommand(r => { if (r is GitHubRepository repo) BrowserHelper.Open(repo.HtmlUrl); });
+        DownloadCommand = new RelayCommand(r => { if (r is GitHubRepository repo) BrowserHelper.Open(repo.ZipUrl); });
+        CopyCloneCommand = new RelayCommand(r =>
         {
             if (r is GitHubRepository repo)
-                BrowserHelper.Open(repo.HtmlUrl);
+            {
+                Clipboard.SetText(repo.CloneUrl);
+                StatusText = "Copied clone URL: " + repo.CloneUrl;
+            }
+        });
+        CategoryCommand = new RelayCommand(async q =>
+        {
+            SearchText = q?.ToString() ?? "windows app";
+            await SearchAsync();
         });
     }
 
     private async Task SearchAsync()
     {
-        Repositories.Clear();
-        var repos = await _service.SearchAsync(SearchText);
-        foreach (var repo in repos)
-            Repositories.Add(repo);
+        try
+        {
+            IsLoading = true;
+            StatusText = "Searching GitHub...";
+            Repositories.Clear();
+            var repos = await _service.SearchAsync(SearchText);
+            foreach (var repo in repos) Repositories.Add(repo);
+            StatusText = $"Found {Repositories.Count} repositories for '{SearchText}'.";
+        }
+        catch (Exception ex)
+        {
+            StatusText = "Search failed: " + ex.Message;
+        }
+        finally
+        {
+            IsLoading = false;
+        }
     }
 
     public event PropertyChangedEventHandler? PropertyChanged;
